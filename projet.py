1d14c1d83af8c12f2345bf98883f04dda1cf13cd
Rana <smichirana4@gmail.com>
2025-03-10 11:27:17 +0100

    Resolved merge conflict in projet.py

--- projet.py
+++ projet.py
@@ -4,13 +4,19 @@ Def somme(T):
         s+=t
         return s
 Data=[1,3,5]
+<<<<<<< HEAD
 som=sum(Data)
 print('la somme est :', som)
 
+=======
+>>>>>>> projeth2
 if Data:
     print('la somme est :', sum(Data))
     print('le min est :', min(Data))
     print('le max est :', max(Data))
 else : 
     print ('dossier vide')
+<<<<<<< HEAD
 
+=======
+>>>>>>> projeth2
