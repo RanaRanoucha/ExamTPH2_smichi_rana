dd4ed32387c272ae50f4942643ce1e3b55034609
Rana <smichirana4@gmail.com>
2025-03-10 11:25:12 +0100

    Resolved merge conflict in projet.py

--- projet.py
+++ projet.py
@@ -7,3 +7,10 @@ Data=[1,3,5]
 som=sum(Data)
 print('la somme est :', som)
 
+if Data:
+    print('la somme est :', sum(Data))
+    print('le min est :', min(Data))
+    print('le max est :', max(Data))
+else : 
+    print ('dossier vide')
+
